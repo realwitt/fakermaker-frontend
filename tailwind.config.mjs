de47439c19128e89e3739801d489a43cbbc38578
Elias Witt <wittbrass@icloud.com>
2024-12-31 18:20:30 -0500

    coming soon page

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+        colors: {
+          'bg': '#212738',
+          'bg-slightly-lighter': '#242A3D',
+          'bg-input': '#333C57',
+          'line': '#424E70',
+          'text-grey': '#9FA9C6',
+          'accent-pink': '#F75590',
+          'accent-purple': '#BDB2FF',
+        }
+    },
+    plugins: [],
+  }
+}
